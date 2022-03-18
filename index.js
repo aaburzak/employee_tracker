15a729de0ca9096f9d31f6b4703031af04ad0941
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-03-18 13:41:33 -0400

    buit out menu function

--- index.js
+++ index.js
@@ -12,3 +12,35 @@ const employee_DataBase = mysql.createConnection(
     },
     console.log('Connected to employeeDB')
 );
+
+menu ()
+
+function menu(){
+    inquirer.prompt([
+        {
+            name: 'menu',
+            type: 'list',
+            message: 'MENU:',
+            choices: ['View All Departments', 'View All Roles', 'View All Employees', 'Add A Department', 'Add A Role', 'Add An Employee', 'Update An Employee Role', 'Quit']
+        }
+    ])
+    .then(({menuChoice}) => {
+        if(menuChoice === 'View All Departments' ){
+            selectDepartment()
+        } else if (menuChoice === 'View All Roles'){
+            selectRole()
+        } else if (menuChoice === 'View All Employees'){
+            selectEmployee()
+        } else if (menuChoice === 'Add A Department'){
+            addDepartment()
+        } else if (menuChoice === 'Add A Role'){
+            addRole()
+        } else if (menuChoice === 'Add An Employee'){
+            addEmployee()
+        } else if (menuChoice === 'Update An Employee Role'){
+            updateRole ()
+        } else {
+            process.exit();
+        }
+    })
+}
