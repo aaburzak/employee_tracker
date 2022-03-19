962b288cb091908e48a37e36b2bdd9afe488134f
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-03-19 18:30:58 -0400

    removed redundant code

--- db/schema.sql
+++ db/schema.sql
@@ -22,5 +22,4 @@ CREATE TABLE employee (
     role_id INT,
     manager_id INT,
     FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE SET NULL
-    -- CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
 );
--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 require("console.table");
-const fs = require("fs");
 const inquirer = require("inquirer");
 const mysql = require("mysql2");
 
