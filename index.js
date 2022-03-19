e6c12dccc01e7a34c9e624848df5e7ace2cdc580
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-03-19 10:50:36 -0400

    fixed connection issue with db,  further built out menu

--- index.js
+++ index.js
@@ -1,74 +1,80 @@
-require('console.table');
-// const fs = require('fs');
-const inquirer = require ('inquirer');
-const mysql = require ('mysql2');
+require("console.table");
+const fs = require("fs");
+const inquirer = require("inquirer");
+const mysql = require("mysql2");
 
 const connect = mysql.createConnection(
-    {
-        host: 'localhost',
-        user: 'root',
-        password: '',
-        database: 'employeeDB'
-    },
-    console.log('Connected to employeeDB')
+  {
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "employeeDB",
+  },
+  console.log("Connected to employeeDB")
 );
 
-function init(){
-    menu();
-};
+function init() {
+  menu();
+}
 
-async function menu(){
-    await inquirer.prompt([
-            {
-                type: "list",
-                name: "userChoice",
-                message:"Menu:",
-                choices: [
-                    "View All Departments",
-                    "View All Roles",
-                    "View All Employees"
-                ]
-            },
-        ])
-        .then(({userChoice}) => {
-            if (userChoice === "View All Departments"){
-                viewDepartment()
-            } else if (role === "View All Roles") {
-                viewRole()
-            } else {
-                viewEmployee()
-            }
-        })
+function menu() {
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "userChoice",
+        message: "Menu:",
+        choices: [
+          "View All Departments",
+          "View All Roles",
+          "View All Employees",
+          "Add A Department",
+          "Add An Employee",
+          "Update An Employee Role",
+          "Quit"
+        ],
+      },
+    ])
+    .then(({ userChoice }) => {
+      if (userChoice === "View All Departments") {
+        viewDepartment();
+      } else if (userChoice === "View All Roles") {
+        viewRole();
+      } else if (userChoice === "View All Employees"){
+        viewEmployee();
+      } else if (userChoice === "View All Employees"){
+        viewEmployee();
+      } else if (userChoice === "Add A Department"){
+        addDepartment();
+      } else if (userChoice === "Add An Employee"){
+        addEmployee();
+      } else if (userChoice === "Update An Employee Role"){
+        updateRole();
+      } else {
+        process.exit();
+      }
+    });
 }
 
 const viewDepartment = () => {
-    connect.query(
-        'SELECT * FROM department;',
-        (err, results) => {
-            console.table(results);
-            menu();
-        }
-    )
+  connect.query("SELECT * FROM department;", (err, results) => {
+    console.table(results);
+    menu();
+  });
 };
 
 const viewRole = () => {
-    connect.query(
-        'SELECT * FROM role;',
-        (err, results) => {
-            console.table(results);
-            menu();
-        }
-    )
+  connect.query("SELECT * FROM role;", (err, results) => {
+    console.table(results);
+    menu();
+  });
 };
 
 const viewEmployee = () => {
-    connect.query(
-        'SELECT * FROM employee;',
-        (err, results) => {
-            console.table(results);
-            menu();
-        }
-    )
+  connect.query("SELECT E.id AS id, E.first_name AS first_name, E.last_name AS last_name, role.title AS title, department.name AS department, role.salary AS salary, CONCAT(M.first_name, ' ', M.last_name) AS manager FROM employee E LEFT JOIN role ON E.role_id = role.id LEFT JOIN department ON role.department_id = department.id LEFT JOIN employee M ON E.manager_id = M.id;  ", (err, results) => {
+    console.table(results);
+    menu();
+  });
 };
 
 init();
